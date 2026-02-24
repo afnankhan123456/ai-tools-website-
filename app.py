89d55a9df82a4b7752327cb2db92e5e64f8be713
afnan khan <afnankhan529683@gmail.com>
2026-02-24 12:57:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def image_resizer():
 # LOCAL RUN
 # ===============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
