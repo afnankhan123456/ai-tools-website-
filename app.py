60c6ad0a4b99ae9ce9e81a919b1215a652c8e44e
afnan khan <afnankhan529683@gmail.com>
2026-05-11 11:18:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from flask import Flask, render_template, request, send_file, jsonify, send_from_directory
+from flask import Flask, render_template, request, send_file, jsonify, send_from_directory, redirect
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
